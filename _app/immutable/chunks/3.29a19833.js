10a6211792868932a5b96386372104920acb7d34
Gregor Parzefall <gregor.parzefall@posteo.de>
2023-04-11 12:57:41 +0200

    Initial commit

---
+++ _app/immutable/chunks/3.29a19833.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/about-page.svelte.87b8682d.js";export{t as component};
---
+++ _app/immutable/chunks/4.dae52fe8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/start-page.svelte.dcc49b27.js";export{t as component};
